0c154f30465614e9f06033da73a67ef2a5315a25
Tiago Rocha <tiagoferreira.info@gmail.com>
2022-04-15 18:44:57 -0300

    Initial commit

---
+++ BackEnd/src/main/java/br/edu/ifpb/dac/trainee/TraineeApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.dac.trainee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraineeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraineeApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/br/edu/ifpb/dac/trainee/TraineeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.dac.trainee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraineeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
